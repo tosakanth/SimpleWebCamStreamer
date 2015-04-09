c57bf3f06cb707d14406d4fb649f90b480498f16
Tosakanth Project <tosakun.meeting@gmail.com>
2015-04-09 23:36:15 +0700

    Update SimpleWebStreamer.py

--- SimpleWebStreamer.py
+++ SimpleWebStreamer.py
@@ -23,6 +23,72 @@ class WebHandler(BaseHTTPRequestHandler):
          self.end_headers()
          while True :
             try :
-               rc,img  = camera
-            except :
+               #capture a picture from web camera
+               rc,img  = camera.read()
+               img_rgb = cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+               
+               #encode it to jpeg format
+               jpg_img =  Image.fromarray(img_rgb)
+               
+               #create in-memory temp file to store jpeg image
+               mem_file = StringIO()
+               
+               #save jpeg on in-memory file
+               jpg_img.save(mem_file,'JPEG')
+               
+               #write boundary string to web page
+               self.wfile.write("--BOUNDARYSTRING")
+               
+               #set content type as image type
+               self.send_header("Content-type","image/jpeg")
+               
+               #tell browser to know size of data
+               self.send_header("Content-length",str(mem_file.len))
+               self.end_headers()
+               
+               #write image content to web browser
+               self.wfile.write(mem_file.getvalue()+"\r\n")
+               
+            except KeyboardInterrupt:
+               break
             
+      elif self.path=="/" or not self.path :
+         self.send_response(200)
+         self.send_header("Content-type","text/html")
+         self.end_headers()
+         self.wfile.write(
+"""
+<html>
+<head>
+<style type=text/css>
+ body { background-image: url(/stream); background-repeat:no-repeat; background-position:center top; background-attachment:fixed; height:100% }
+</style>
+</head>
+<body>
+</body>
+</html>
+"""
+         )
+         
+camera = None
+cam_index=0 #/dev/video0
+port = 8080
+
+def main():
+   global camera
+   global cam_index
+   global port   
+   camera = cv2.VideoCapture(cam_index)
+   camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,320)      
+   camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,240)      
+   try:
+      webserver = HTTPServer(('0.0.0.0',port),WebHandler)
+      print "Server Started"
+      webserver.serve_forever()
+   except:
+      camera.release()
+      webserver.socket.close()
+      
+if __name__ == "__main__":
+   main()
+
